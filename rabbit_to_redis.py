735acfd9d58ac40ad4333c29bf4314cd471e8305
filipok <filipok.yul@gmail.com>
2021-05-12 15:35:36 +0300

    Updated: to rabbit_to_redis.py added new feature to prevent it from multiple launch while using with cron.

--- rabbit_to_redis.py
+++ rabbit_to_redis.py
@@ -1,5 +1,8 @@
 import pika
 import redis
+import os
+import sys
+
 
 def callback(ch, method, properties, body):
     data = body.decode().split('|')
@@ -9,15 +12,27 @@ def callback(ch, method, properties, body):
     r.rpush(weather_key, weather_value)
     print([weather_key, weather_value])
 
-def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-    channel = connection.channel()
-    channel.queue_declare(queue='w')
-    channel.basic_consume(queue='w', on_message_callback=callback, auto_ack=True)
 
-    print(' [*] Waiting for messages. To exit press CTRL+C')
-    channel.start_consuming()
-    #channel.stop_consuming()
+def main():
+    pid = str(os.getpid())
+    pidfile = "/tmp/mydaemon.pid"
+
+    if os.path.isfile(pidfile):
+        print("Script is already on")
+        sys.exit()
+    open(pidfile, 'w').write(pid)
+    try:
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        channel = connection.channel()
+        channel.queue_declare(queue='w')
+        channel.basic_consume(queue='w', on_message_callback=callback, auto_ack=True)
+
+        print(' [*] Waiting for messages. To exit press CTRL+C')
+        channel.start_consuming()
+        # channel.stop_consuming()
+    finally:
+        os.unlink(pidfile)
+
 
 if __name__ == '__main__':
     main()
